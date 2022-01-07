841acf71b84b28418d7b6d501f91870f0b6183f5
Jérémy <jeremy.metra@cpe.fr>
2022-01-07 08:56:09 +0100

    nouvelle branche docker

--- fiveserver.config.js
+++
@@ -1,7 +0,0 @@
-//config du Online Server.
-module.exports = {
-    open: "index.php",
-    php: "/usr/local/bin/php", // macOS/Ubuntu
-    remoteLogs: true, // enable remoteLogs
-    remoteLogs: "yellow" // enable remoteLogs and use the color yellow
-} 
